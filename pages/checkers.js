bba7bb64bcf02e0dc94a1a7f6d01ee033fbed7ec
Nafis Fuad <fuadbd14@gmail.com>
2019-08-11 18:11:25 +0600

    Add checkers page

---
+++ pages/checkers.js
@@ -0,0 +1,11 @@
+import Checkerboard from '../components/Checkerboard'
+import GithubCorner from '../components/GithubCorner'
+
+const Page = () => (
+  <div>
+    <GithubCorner />
+    <Checkerboard dimensions={12} />
+  </div>
+)
+
+export default Page
