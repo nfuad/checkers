164c0a778a459b6230866e8d7562f2b4840a6e33
Nafis Fuad <fuadbd14@gmail.com>
2019-08-13 15:55:44 +0600

    Update Footer Add new icon Update color Use media queries to make responsive

--- components/Footer.js
+++ components/Footer.js
@@ -1,31 +1,59 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faFileCode } from '@fortawesome/free-solid-svg-icons'
 import { Fade } from 'react-reveal'
 
 const Footer = () => (
   <Fade bottom>
     <p>
-      Designed &amp; Developed with{' '}
+      Designed and{' '}
       <span className="icon">
+        <FontAwesomeIcon icon={faFileCode} className="fa-2x" />{' '}
+      </span>{' '}
+      with{' '}
+      <span className="heart-icon">
         <FontAwesomeIcon icon={faHeart} className="fa-2x" />{' '}
       </span>{' '}
       by{' '}
       <a href="https://nafis.co" target="_blank">
-        Nafis Fuad
+        Nafis Fuad.
       </a>
+      <br />
+      <span className="copyleft">&copy;</span> Copyleft. No Rights Reserved.{' '}
+      {new Date().getFullYear()}
     </p>
 
     <style jsx>{`
+      p,
+      a,
+      span {
+        font-size: 14px;
+        font-family: 'Nunito', cursive;
+      }
       p {
         text-align: center;
         opacity: 0; // keep initial opacity to zero for fade in animation
+        padding-top: 70px;
+        padding-bottom: 20px;
       }
 
-      p > span.icon {
+      p > span.icon,
+      p > span.heart-icon {
         display: inline-block;
+        width: 15px;
+        height: 15px;
+        color: darkgrey;
+      }
+
+      p > span.heart-icon {
         width: 20px;
         height: 20px;
         animation: pulse 0.5s infinite;
+      }
+
+      .copyleft {
+        display: inline-block;
+        transform: rotate(180deg);
+        font-size: 16px;
       }
 
       @keyframes pulse {
@@ -37,6 +65,14 @@ const Footer = () => (
         }
         100% {
           transform: scale(1.2);
+        }
+      }
+
+      @media only screen and (max-width: 500px) {
+        p,
+        a,
+        span {
+          font-size: 12px;
         }
       }
     `}</style>
